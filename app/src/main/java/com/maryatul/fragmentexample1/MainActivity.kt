ae105ffdb6e0367d1e31d785b48577666d2852f9
JALA MEDIA COMP <maryatulkippiyah01@gmail.com>
2022-05-20 15:33:08 +0700

    Percobaan fragment example 2

--- app/src/main/java/com/maryatul/fragmentexample1/MainActivity.kt
+++ app/src/main/java/com/maryatul/fragmentexample1/MainActivity.kt
@@ -1,18 +1,21 @@
 package com.maryatul.fragmentexample1
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import kotlinx.android.synthetic.main.activity_main.*
-
+import androidx.appcompat.app.AppCompatActivity
 class MainActivity : AppCompatActivity(), Coordinator {
+    lateinit var fragBookDescription: BookDescription
+    lateinit var fragBookTitle: BookTitle
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        fragBookTitle = BookTitle()
+        fragBookDescription = BookDescription()
+        val fragTransaction = supportFragmentManager.beginTransaction()
+        fragTransaction.add(R.id.fragtop, fragBookTitle)
+        fragTransaction.add(R.id.fragbottom, fragBookDescription)
+        fragTransaction.commit()
     }
     override fun onBookChanged(index:Int) {
-        val frag = fragmentbookdescription
-        if (frag is BookDescription) {
-            frag.changeDescription(index)
-        }
+        fragBookDescription.changeDescription(index)
     }
 }
